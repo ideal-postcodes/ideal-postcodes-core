982fa549fb450b20c324776f0936302fa592de52
Chris Blanchard <cablanchard@gmail.com>
2017-02-21 10:47:00 +0000

    Lint

--- lib/index.ts
+++ lib/index.ts
@@ -4,9 +4,9 @@ namespace IdealPostcodes {
 	export const VERSION = "v1";
 	export const DEFAULT_TIMEOUT = 10000;
 
-	/* 
+	/*
 	 * STRICT_AUTHORISATION forces authorization header usage on
-	 * autocomplete API which increases latency due to overhead 
+	 * autocomplete API which increases latency due to overhead
 	 * OPTIONS request
 	 */
 	export const STRICT_AUTHORISATION = false;
